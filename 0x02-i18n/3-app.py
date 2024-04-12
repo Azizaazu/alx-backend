2b3cdbdefb90d37a8280bd5247c255d59f72b02c
Azizaazu <aynaou.aziza98@gmail.com>
2024-04-12 13:14:07 -0700

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 @app.route('/')
-def index() -> str:
+def get_index() -> str:
     """
     Renders index.html template
     """
