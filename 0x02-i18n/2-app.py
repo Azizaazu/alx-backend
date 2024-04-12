9def664dbb1907587a17fc93b3d3121ebba032ec
Azizaazu <aynaou.aziza98@gmail.com>
2024-04-12 13:28:04 -0700

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,13 +3,12 @@
 2-app.py
 Basic Flask app with a single route '/'
 """
-from flask import Flask, render_template, request
+from flask import (
+        Flask,
+        render_template,
+        request
+)
 from flask_babel import Babel
-
-
-app = Flask(__name__)
-babel = Babel(app)
-app.config.from_object(Config)
 
 
 class Config:
@@ -17,8 +16,21 @@ class Config:
     Config class for Flask app
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get locale based on request.accept_languages
+    """
+    return request.accept_languages.best_match(app.onfig['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
@@ -29,13 +41,6 @@ def index() -> str:
     return render_template('2-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """
-    Get locale based on request.accept_languages
-    """
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
