aa7ed6834bb9b02240cc0eaf54fc9ad8c8d25d94
Azizaazu <aynaou.aziza98@gmail.com>
2024-04-12 13:03:00 -0700

    update

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,13 +6,14 @@ Basic Flask app with a single route '/'
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config(object):
     """
     Config class for Flask app
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -29,4 +30,4 @@ def index() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
