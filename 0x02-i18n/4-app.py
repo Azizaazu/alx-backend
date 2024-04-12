289cba86db690a8c2d692f5b0e1c69fd12516f4e
Azizaazu <aynaou.aziza98@gmail.com>
2024-04-12 15:26:12 -0700

    update

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 """
-0-app.py
+4-app.py
 Basic Flask app with a single route '/'
 """
-from flask import Flask, render_template, request
+from flask import (
+        Flask,
+        render_template,
+        request
+)
 from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Config class for Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
-
-
-class Config:
-    """
-    Config class for Flask app
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-@app.route('/')
-def index() -> str:
-    """
-    Renders index.html template
-    """
-    return render_template('4-index.html')
 
 
 @babel.localeselector
@@ -40,5 +36,13 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/')
+def index() -> str:
+    """
+    Renders index.html template
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
